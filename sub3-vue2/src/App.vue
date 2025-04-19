fe5bf8d3e648e891b59761f3fb4a2ba3a1ecf998
lengfangbing <lengfangbing@gmail.com>
2025-04-20 01:18:01 +0800

    feat: 完成所有页面最简代码

--- sub3-vue2/src/App.vue
+++ sub3-vue2/src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="content">
     <h1>Rsbuild with Vue</h1>
-    <p>Start building amazing things with Rsbuild.</p>
+    <p>Start building amazing things with Rsbuild. vue3子应用的vue2子应用</p>
+    <button @click="jumpHome">跳转home页面</button>
+    <button @click="jumpAbout">跳转about页面</button>
+    <router-view></router-view>
   </div>
 </template>
-
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log(this.$router);
+  },
+  methods: {
+    jumpHome() {
+      this.$router.push({
+        path: '/home',
+      });
+    },
+    jumpAbout() {
+      this.$router.push({
+        path: '/about',
+      });
+    },
+  },
+};
+</script>
 <style scoped>
 .content {
   display: flex;
---
+++ sub3-vue2/src/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">我是Home组件</div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+.home {
+  color: aqua;
+  width: 150px;
+  background: #020917;
+}
+</style>
