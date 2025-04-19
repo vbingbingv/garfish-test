59b99f2ed0a6bdf7ac060c16d8666b758a9d226f
lengfangbing <lengfangbing@gmail.com>
2025-04-19 12:16:57 +0800

    feat: 增加sub2-vue3的代码

--- sub2-vue3/rsbuild.config.ts
+++ sub2-vue3/rsbuild.config.ts
@@ -3,6 +3,19 @@ import { pluginVue } from '@rsbuild/plugin-vue';
 
 export default defineConfig({
   plugins: [pluginVue()],
+  tools: {
+    rspack: {
+      output: {
+        // 需要配置成 umd 规范
+        library: {
+          type: 'umd',
+        },
+        // 修改不规范的代码格式，避免逃逸沙箱
+        globalObject: 'window',
+        chunkLoadingGlobal: 'sub1-react-sub-vue3',
+      },
+    },
+  },
   server: {
     port: 3004,
   },
--- sub2-vue3/src/index.ts
+++ sub2-vue3/src/index.ts
@@ -1,9 +1,36 @@
+import { vueBridge } from '@garfish/bridge-vue-v3';
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import About from './About.vue';
 import App from './App.vue';
 import './index.css';
+import Home from './Home.vue';
+import Router from './Router.vue';
+
+const routes = [
+  { path: '/', component: Router },
+  { path: '/home', component: Home },
+  { path: '/about', component: About },
+];
+
+function newRouter(basename: string) {
+  console.log('11111 basename', basename);
+  return createRouter({
+    history: createWebHistory(basename),
+    routes,
+  });
+}
+
+export const provider = vueBridge({
+  rootComponent: App,
+  // 可选，注册 vue-router或状态管理对象
+  handleInstance: (vueInstance, { basename }) => {
+    vueInstance.use(newRouter(basename));
+  },
+});
 
 try {
-  createApp(App).mount(`#${process.env.PUBLIC_MOUNT_ID}`);
+  createApp(App).use(newRouter('/')).mount(`#${process.env.PUBLIC_MOUNT_ID}`);
 } catch (e) {
   console.error('single mount error');
 }
