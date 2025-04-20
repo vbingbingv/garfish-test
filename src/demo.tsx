3209ef79d8433a31de7cc341c916a82e60dcdd8b
lengfangbing <lengfangbing@gmail.com>
2025-04-20 21:59:28 +0800

    feat: 优化demo示例

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,28 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <div>
-      <Link to="sub1">
-        子应用1
-      </Link>
-      <Link to="sub2">
-        子应用2
-      </Link>
+      <Link to="sub1">子应用1</Link>
+      <Link to="sub2">子应用2</Link>
+      <Link to="sub3">子应用3</Link>
     </div>
-  )
+  );
 }
 
 export function Sub1() {
-  return (
-    <div id="sub1">
-    </div>
-  )
+  return <div id="sub1"></div>;
 }
 
 export function Sub2() {
-  return (
-    <div id="sub2">
-    </div>
-  )
+  return <div id="sub2"></div>;
+}
+
+export function Sub3() {
+  return <div id="sub3"></div>;
 }
